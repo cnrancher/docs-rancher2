3d7aa5c1dda69ec273ab4baec522dd6ef9d40cec
yilin <yilin@yilinrancher.local>
2020-06-16 17:41:13 +0800

    update baseCommit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,49 +12,49 @@ module.exports = {
             title: "Rancher 2.x",
             logo: {
                 alt: "Rancher Logo",
-                src: "img/rancher-logo-cow-white.svg"
+                src: "img/rancher-logo-cow-white.svg",
             },
             links: [{
                     href: "https://docs.rancher.cn/",
                     label: "文档中心",
-                    position: "left"
+                    position: "left",
                 },
                 { to: "pdf", label: "获取 PDF 文档", position: "left" },
                 {
                     href: "https://www.rancher.cn/weixin/",
                     label: "微信",
-                    position: "left"
+                    position: "left",
                 },
                 {
                     href: "https://www.rancher.cn/",
                     label: "中国官网",
-                    position: "right"
+                    position: "right",
                 },
                 {
                     href: "https://rancher.com/support-maintenance-terms/all-supported-versions/",
                     label: "支持矩阵",
-                    position: "right"
+                    position: "right",
                 },
                 {
                     href: "https://www.rancher.cn/support/",
                     label: "技术支持",
-                    position: "right"
+                    position: "right",
                 },
                 {
                     href: "https://github.com/rancher/rancher",
                     label: "GitHub",
-                    position: "right"
-                }
-            ]
+                    position: "right",
+                },
+            ],
         },
         algolia: {
             apiKey: "f790c2168867f49bb212aee8c224116d",
-            indexName: "rancher"
+            indexName: "rancher",
         },
         footer: {
             style: "dark",
-            copyright: `Copyright © ${new Date().getFullYear()} Rancher Labs, Inc. All Rights Reserved. 粤ICP备16086305号`
-        }
+            copyright: `Copyright © ${new Date().getFullYear()} Rancher Labs, Inc. All Rights Reserved. 粤ICP备16086305号`,
+        },
     },
     presets: [
         [
@@ -64,19 +64,19 @@ module.exports = {
                     sidebarPath: require.resolve("./sidebars.js"),
                     editUrl: "https://github.com/cnrancher/docs-rancher2/edit/dev/",
                     showLastUpdateAuthor: true,
-                    showLastUpdateTime: true
+                    showLastUpdateTime: true,
                 },
                 theme: {
-                    customCss: require.resolve("./src/css/custom.css")
-                }
-            }
-        ]
+                    customCss: require.resolve("./src/css/custom.css"),
+                },
+            },
+        ],
     ],
     customFields: {
         sidebars,
         metadata,
         stable: "版本说明 - v2.4.4",
-        baseCommit: "4eeb540544444f3868be92dd05271b385c0cdcc3 - June 9, 2020",
-        rkeBaseCommit: "29992bb2ff420d53ca6c51b6520fe97a13c46c2d - June 4, 2020"
-    }
+        baseCommit: "228d9b0e87d5cf0c6f8965e71c75f120490a1360 - June 14, 2020",
+        rkeBaseCommit: "29992bb2ff420d53ca6c51b6520fe97a13c46c2d - June 4, 2020",
+    },
 };
