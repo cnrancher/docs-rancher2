a88b3e11376aa6ed6e3f2c93737cf4fcf2f2e0ee
yzeng25 <36651058+yzeng25@users.noreply.github.com>
2021-03-08 18:11:08 +0800

    add release note for 2.5.6

--- sidebars.js
+++ sidebars.js
@@ -8,6 +8,7 @@
 module.exports = {
     rancher2: {
         版本说明: [
+            "rancher2/releases/v2.5.6",
             "rancher2/releases/v2.5.5",
             "rancher2/releases/v2.5.4",
             "rancher2/releases/v2.5.3",
